3092fa562704bb84b302dc1d2f4570bd1f20598d
Zack Buhman <zack@buhman.org>
2020-12-22 14:56:37 -0800

    day8: part2

--- day8.lisp
+++ day8.lisp
@@ -11,25 +11,59 @@
        ((string= opcode "nop") 'nop))
      (parse-integer operand))))
 
-(defun console-eval (mem seen ip acc)
-  (let* ((ins (elt mem ip))
-         (opcode (car ins))
-         (operand (cdr ins)))
-    (if (or (member ip seen) (>= ip (length mem)))
-      acc
-      (apply #'console-eval
-             mem
-             (cons ip seen)
-             (case opcode
-               ((acc)
-                (list (+ ip 1) (+ acc operand)))
-               ((jmp)
-                (list (+ ip operand) acc))
-               ((nop)
-                (list (+ ip 1) acc)))))))
+(defun console-eval (mem acc ip seen)
+  (if (or (member ip seen) (= ip (length mem)))
+    (values acc ip seen)
+    (let* ((ins (elt mem ip))
+           (opcode (car ins))
+           (operand (cdr ins)))
+      (console-eval
+       mem
+       (case opcode
+         ((acc) (+ acc operand))
+         ((jmp nop) acc))
+       (case opcode
+         ((acc nop) (+ ip 1))
+         ((jmp) (+ ip operand)))
+       (cons ip seen)))))
+
+(defun read-input ()
+  (map 'vector
+       #'parse-line
+       (read-lines-from-file "day8.input")))
 
 (defun part1 ()
-  (let ((mem (map 'vector
-                  #'parse-line
-                  (read-lines-from-file "day8.input"))))
-    (console-eval mem '() 0 0)))
+  (let ((mem (read-input)))
+    (multiple-value-bind (ip) (console-eval mem 0 0 '())
+      ip)))
+
+(defun flip-opcode (mem i)
+  (let* ((ins (elt mem i))
+         (opcode (car ins))
+         (operand (cdr ins)))
+    (setf (elt mem i)
+          (cons
+           (case opcode
+             ((jmp) 'nop)
+             ((nop) 'jmp))
+           operand))))
+
+(defun non-acc-addrs (mem addrs)
+  (remove-if (lambda (i) (eq (car (elt mem i)) 'acc)) addrs))
+
+(defun find-terminating (mem addrs)
+  (let ((i (car addrs)))
+    (flip-opcode mem i)
+    (multiple-value-bind (acc ip) (console-eval mem 0 0 '())
+      (if (= ip (length mem))
+        acc
+        (progn
+         (flip-opcode mem i)
+         (find-terminating mem (cdr addrs)))))))
+
+(defun part2 ()
+  (let ((mem (read-input)))
+    (multiple-value-bind (acc ip seen) (console-eval mem 0 0 '())
+      (declare (ignore acc ip))
+      (let ((addrs (non-acc-addrs mem seen)))
+        (find-terminating mem addrs)))))
