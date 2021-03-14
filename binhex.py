0cb650628fd033f19ec164074bcbda0c1219ff96
Zack Buhman <zack@buhman.org>
2021-03-14 01:38:58 -0800

    binhex.py: pad sub-word reads

--- binhex.py
+++ binhex.py
@@ -6,7 +6,8 @@ size = int(sys.argv[2])
 
 with open(sys.argv[1], 'rb') as f:
     while ((buf := f.read(4)) != b''):
-        sys.stdout.write(hexlify(bytes(reversed(buf))).decode('utf-8'))
+        sys.stdout.write('ee' * (4 - len(buf)) +
+                         hexlify(bytes(reversed(buf))).decode('utf-8'))
         sys.stdout.write('\n')
         index += 1
     assert index < size, index
