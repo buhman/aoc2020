687bdfb9cdea869137733b5bbbf863aabcc25bee
Zack Buhman <zack@buhman.org>
2021-03-21 23:53:24 -0700

    day1: remove redundant branches
    This was originally a literal translation of:
    
      while (1) {
        if (foo);
          break;
      }
    
    However, a 1:1 translation of this requires 2 branch instructions, even though 1
    is sufficient.
    
    IPC is reduced in both implementations because the bubbles caused by load
    instructions are becoming more dominant in the average.

--- day1/part1.s
+++ day1/part1.s
@@ -1,15 +1,15 @@
 /*
 single-stage:
-  instret = 36111
-  cycle = 42141
+  instret = 30116
+  cycle = 36146
 
-  0.857 instructions/cycle
+  0.833 instructions/cycle
 
 pipelined:
-  instret = 36108
-  cycle = 42208
+  instret = 30113
+  cycle = 36214
 
-  0.855 instructions/cycle
+  0.831 instructions/cycle
 */
 
         .section .text.vector
@@ -23,8 +23,8 @@ _start:
         addi x4,x0,2020  /* desired sum */
 
         addi x1,x0,0
+        bge x1,x31,_forever
 _loop1:
-        beq x1,x31,_forever
         addi x2,x1,4
         lw x10,0(x1)
 _loop2:
@@ -32,13 +32,14 @@ _loop2:
 
         add x12,x10,x11
         beq x12,x4,_found
-_inc11:
         addi x2,x2,4
-        beq x2,x31,_inc10
-        jal x0,_loop2
-_inc10:
+        blt x2,x31,_loop2
+
         addi x1,x1,4
-        jal x0,_loop1
+        blt x1,x31,_loop1
+
+_forever:
+        jal x0,_forever
 
 _found:
         /*mul x0,x10,x11*/
@@ -46,5 +47,4 @@ _found:
         addi x0,x10,0
         csrrs x30,minstret,x0
         csrrs x30,mcycle,x0
-_forever:
         jal x0,_forever
