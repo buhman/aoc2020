ca998576b91a335740dddb938f7aac5ca6adc0c6
Zack Buhman <zack@buhman.org>
2021-08-29 17:09:43 -0700

    add archive build
    This makes the solution easier to link from external code.
    
    Also add minor changes to save/restore the link register, so that the day1_part1
    symbol can be called with `bl`.

--- common.mk
+++ common.mk
@@ -1,28 +1,33 @@
 ARCH = -mthumb-interwork -march=armv4t
 override CFLAGS += -Wall -mtune=arm7tdmi -ffreestanding -nostdlib -g
+#DEFS = -D_START
 ASFLAGS = -g
 
 TARGET = arm-none-eabi-
 CC = $(TARGET)gcc
 AS = $(TARGET)as
 LD = $(TARGET)ld
+AR = $(TARGET)ar
 OBJCOPY = $(TARGET)objcopy
 OBJDUMP = $(TARGET)objdump
 
 %.o: %.S
-	$(CC) $(ARCH) $(ASFLAGS) -c $< -o $@
+	$(CC) $(ARCH) $(ASFLAGS) $(DEFS) -c $< -o $@
 
 %.o: %.s
 	$(AS) $(ARCH) $(ASFLAGS) $< -o $@
 
 %.o: %.c
-	$(CC) $(ARCH) $(CFLAGS) $< -o $@
+	$(CC) $(ARCH) $(CFLAGS) $(DEFS) $< -o $@
 
 dump:
 	arm-none-eabi-objdump -M reg-names-raw -D -j .text $(F)
 
 a.out:
 	$(LD) $^ -o $@
+
+%.a:
+	$(AR) rcs $@ $^
 
 clean:
 	rm -f *.o *.out *.hex *.elf
